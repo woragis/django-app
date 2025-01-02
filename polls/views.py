e09688d89fc8e7f8c595989f27506e8acaaaad2b
woragis <masteringthecode.woragis+github@gmail.com>
2025-01-02 02:48:13 -0300

    Create better views

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,9 @@
+from django.db.models import F
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from .models import Question, Choice
 from django.template import loader
+from django.urls import reverse
 
 # Create your views here.
 
@@ -27,10 +29,22 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You are looking at the results of the question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 
 def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html", {
+            "question": question,
+            "error_message": "You didn't selected a choice"
+        })
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:result", args=(question.id)))
     response = "You are voting on question %s."
     return HttpResponse(response % question_id)
